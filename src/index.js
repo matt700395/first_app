8477ff09668a58c4a24090344996bf003b138737
matt700395 <86474039+matt700395@users.noreply.github.com>
2022-10-14 16:06:18 +0900

    221014 clock

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import Library from './fisrt_proj/Library';
+import Clock from './clock_proj/clock';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Library />
-  </React.StrictMode>
-);
 
+setInterval(()=>{
+  root.render(
+    <React.StrictMode>
+      <Clock />
+    </React.StrictMode>
+  );
+})
 
 
 
@@ -23,13 +24,12 @@ root.render(
 
 
 {/*
-setInterval(() => {
-  root.render(
-    <React.StrictMode>
-      <Clock />
-    </React.StrictMode>
-  );
-})
+root.render(
+  <React.StrictMode>
+    <Library />
+  </React.StrictMode>
+);
+
 */}
 
 reportWebVitals();
