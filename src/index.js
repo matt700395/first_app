88ac950937af4e9d76f6314931a31bfc2de2ff2a
matt700395 <86474039+matt700395@users.noreply.github.com>
2022-10-14 15:04:52 +0900

    221014 수업

--- src/index.js
+++ src/index.js
@@ -4,14 +4,32 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Library from './fisrt_proj/Library';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Library />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+
+
+
+
+
+
+
+
+{/*
+setInterval(() => {
+  root.render(
+    <React.StrictMode>
+      <Clock />
+    </React.StrictMode>
+  );
+})
+*/}
+
 reportWebVitals();
